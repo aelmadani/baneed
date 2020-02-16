3c997fefec41eed549ec256bfa060094086d141d
Ahmad Madani <ahmadmadani@Ahmads-MacBook-Pro.local>
2020-02-16 23:55:10 +0100

    Preparing for heroku dep

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ carRoutes(app);
 profileRoutes(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("../../client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
