a1e23d4e9c839159a45002e174d36c92dc3b816c
Ahmad Madani <ahmadmadani@Ahmads-MacBook-Pro.local>
2020-02-17 21:38:11 +0100

    changing facebook params for production

--- client/src/components/layoutComponents/FacebookBtn/FacebookBtn.js
+++ client/src/components/layoutComponents/FacebookBtn/FacebookBtn.js
@@ -1,11 +1,13 @@
 import React from "react";
+const config = require("config");
+
 import { Link } from "react-router-dom";
 
 import "./FacebookBtn.css";
 
 const FacebookBtn = () => {
   return (
-    <form action="http://localhost:5060/auth/facebook">
+    <form action={config.get("facebookAuthUrl")}>
       <button className="loginBtn loginBtn--facebook">
         Login with Facebook
       </button>
